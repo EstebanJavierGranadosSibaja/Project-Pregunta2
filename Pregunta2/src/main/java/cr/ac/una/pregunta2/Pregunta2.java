32c5935c8d7fa901bab07d80622e72c0d329ee4f
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-04-28 16:55:22 -0600

    Add Pregunta2 project

---
+++ Pregunta2/src/main/java/cr/ac/una/pregunta2/Pregunta2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.una.pregunta2;
+
+/**
+ *
+ * @author esteb
+ */
+public class Pregunta2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
