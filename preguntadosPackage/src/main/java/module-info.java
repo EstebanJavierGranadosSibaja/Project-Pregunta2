b07a3d657ea29cdd66426b08e1b0a08da42a635b
Matteo Vargas <142123012+matteoquesada@users.noreply.github.com>
2024-05-07 18:56:05 -0600

    Add basic resources

--- preguntadosPackage/src/main/java/module-info.java
+++ preguntadosPackage/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module cr.ac.una.preguntadospackage {
     requires java.logging;
     
     requires MaterialFX;
+    requires java.base;
 
     opens cr.ac.una.preguntadospackage to javafx.fxml;
     exports cr.ac.una.preguntadospackage;
