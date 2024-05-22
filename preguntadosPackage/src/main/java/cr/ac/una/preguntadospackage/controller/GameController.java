b1ab647bf85d431c72d05a91e8a5593c44206c6f
Matteo Vargas <142123012+matteoquesada@users.noreply.github.com>
2024-05-22 17:09:37 -0600

    Update GameController.java syntaxis and optimization

--- preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/controller/GameController.java
+++ preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/controller/GameController.java
@@ -23,6 +23,8 @@ public class GameController extends Controller implements Initializable {
     @FXML private Label lblPlayerCurrentlySelecting;
     @FXML private AnchorPane apSelectionScreen;
 
+    // this method takes care of setting up the player count and the help assistance level
+    // TODO: implement the help assistance level and initialize the inventory slots
     public void setupPlayerCount(int playerCount, int helpAssistanceLevel) {
         switch(playerCount) {
             case 2: hboxInventory1.setVisible(true); hboxInventory6.setVisible(true); break;
@@ -31,13 +33,19 @@ public class GameController extends Controller implements Initializable {
             case 5: hboxInventory1.setVisible(true); hboxInventory6.setVisible(true); hboxInventory3.setVisible(true); hboxInventory4.setVisible(true); hboxInventory2.setVisible(true); break;
             case 6: hboxInventory1.setVisible(true); hboxInventory6.setVisible(true); hboxInventory3.setVisible(true); hboxInventory4.setVisible(true); hboxInventory5.setVisible(true); hboxInventory2.setVisible(true); break;
         }
+        // disale the non avaible pawns (they will and can show up in the future, its just a method to disable them for now)
         disableNonAvaiblePawns(playerCount);
+
+        // TODO: for now just disable the Anchor Pane that shows the player selection screen
         apSelectionScreen.setVisible(false);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        hboxInventory1.setVisible(false); hboxInventory2.setVisible(false); hboxInventory3.setVisible(false); hboxInventory4.setVisible(false); hboxInventory5.setVisible(false); hboxInventory6.setVisible(false);
+
+        // hide the non avaible inventory slots (the ones avaible will show up in the setupPlayerCount method)
+        hideNonAvaibleInventorySlots();
+        // hide the category animations and its shadows
         hideCategoryAnimation();
     }
 
@@ -45,6 +53,11 @@ public class GameController extends Controller implements Initializable {
     public void initialize() {
     }
 
+    private void hideNonAvaibleInventorySlots() {
+        hboxInventory1.setVisible(false); hboxInventory2.setVisible(false); hboxInventory3.setVisible(false); hboxInventory4.setVisible(false); hboxInventory5.setVisible(false); hboxInventory6.setVisible(false);
+    }
+
+    // this method takes care of disabling the pawns that are not avaible (this disables the dark pawns the represent the disabled or already selected pawns)
     public void disableNonAvaiblePawns(int playerCount) {
         imgDisabledGreenPawn.setVisible(false);
         imgDisabledOrangePawn.setVisible(false);
@@ -52,6 +65,8 @@ public class GameController extends Controller implements Initializable {
         imgDisabledRedPawn.setVisible(false);
     }
 
+    // this method takes care of showing the player that is currently selecting a pawn
+    // TODO: make it better
     public void playCategoryAnimation(int category) {
         switch(category) {
             case 0: imgScience.setVisible(true); imgScienceShadow.setVisible(true); break;
@@ -63,56 +78,85 @@ public class GameController extends Controller implements Initializable {
         }
     }
 
+    // this method takes care of hiding the category animations and its shadows
     private void hideCategoryAnimation() {
         imgArt.setVisible(false); imgArtShadow.setVisible(false); imgCrown.setVisible(false); imgCrownShadow.setVisible(false); imgEntertainment.setVisible(false); imgEntertainmentShadow.setVisible(false); imgGeography.setVisible(false); imgGeographyShadow.setVisible(false); imgHistory.setVisible(false); imgHistoryShadow.setVisible(false); imgScience.setVisible(false); imgScienceShadow.setVisible(false); imgSport.setVisible(false); imgSportShadow.setVisible(false);
     }
 
+    // this method takes care of showing the player that is currently selecting a pawn
     @FXML
     public void onActionSpinner(MouseEvent event) {
+
+
+        // this is because the roulette is a little bit off center
+        imgSpinner.setRotate(27);
+
+        // this is to prevent the user from clicking the spinner multiple times
         if (hasSpinnerBeenClicked) {
             return;
         }
-        System.out.println("Spinner clicked");
-        imgSpinner.setRotate(27);
-        RotateTransition rt = new RotateTransition();
-        rt.setDuration(javafx.util.Duration.seconds(4.5));
-        rt.setNode(imgSpinner);
+
+        // generate a random number between 0 and 360
         double randomDegrees = Math.random() * 360;
-        System.out.println("Random degrees: " + randomDegrees);
+        // get that and divite it by 51.4 to get the category
         int category = (int) Math.floor(randomDegrees / 51.4);
-        playCategoryAnimation(category);
+
+        // create the animation itself
+        RotateTransition rt = new RotateTransition(javafx.util.Duration.seconds(4.5), imgSpinner);
+        // set the angle to rotate (random degrees + 3600 to make it spin multiple times, exactly 10)
         rt.setByAngle(randomDegrees + 3600);
+
+        // start the animation
         rt.play();
+
+        // show the category logo and shadow
+        // TODO: fix this and make it pop out
+        playCategoryAnimation(category);
+
+        // lambda function to execute when the animation is finished
         rt.setOnFinished(e -> {
-            System.out.println("SetonFinished");
+            // make the current main thread sleep for 2.5 seconds to show the category logo and shadow
             try {
                 Thread.sleep(2500);
             } catch (InterruptedException ex) {
-                System.out.println("Error: " + ex.getMessage());
+                System.out.println("Error al pausar el thread: " + ex.getMessage());
             }
+
+            // get a QuestionController instance and set the category theme to prepare the questions
             QuestionController questionController = (QuestionController) FlowController.getInstance().getController("QuestionView");
+            // set the category theme
             questionController.setCategoryTheme(category);
+
+            // go to the question view (it already has the question theme set)
             FlowController.getInstance().goView("QuestionView");
+
+            // reset the spinner clickability (at this point the user is on the question view)
             hasSpinnerBeenClicked = false;
+
+            // hide the category logo and shadow
             hideCategoryAnimation();
         });
     }
 
+    // this method takes care of showing the player that is currently selecting a pawn
     @FXML
     public void onActionGreenPawnSelected(Event event) {
         System.out.println("Green pawn selected");
     }
 
+    // this method takes care of showing the player that is currently selecting a pawn
     @FXML
     public void onActionOrangePawnSelected(Event event) {
         System.out.println("Orange pawn selected");
     }
 
+    // this method takes care of showing the player that is currently selecting a pawn
     @FXML
     public void onActionPurplePawnSelected(Event event) {
         System.out.println("Purple pawn selected");
     }
 
+    // this method takes care of showing the player that is currently selecting a pawn
     @FXML
     public void onActionRedPawnSelected(Event event) {
         System.out.println("Red pawn selected");
