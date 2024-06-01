dae037b1666f5847f520fc30a0a3bcdeaca4444e
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-05-31 20:09:53 -0600

    Add more changes
    Co-Authored-By: Matteo Vargas <142123012+matteoquesada@users.noreply.github.com>

--- preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/service/PregJugadoresService.java
+++ preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/service/PregJugadoresService.java
@@ -19,9 +19,23 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PregJugadoresService {
-    
-     EntityManager em = EntityManagerHelper.getInstance().getManager();
+
+    EntityManager em = EntityManagerHelper.getInstance().getManager();
     private EntityTransaction et;
+
+    public Boolean validarJugador(String nombre) {
+        Query qryJugador = em.createNamedQuery("PregJugadores.findByJugNombre", PregJugadores.class);
+        qryJugador.setParameter("nombre", nombre);
+        PregJugadoresDto pregJugadoresDto = new PregJugadoresDto((PregJugadores) qryJugador.getSingleResult());
+
+        if (pregJugadoresDto.getId() == null) {
+            return false;
+        }
+        if (pregJugadoresDto.getNombre().isEmpty() || pregJugadoresDto.getNombre().isBlank()) {
+            return true;
+        }
+        return false;
+    }
 
     public Respuesta getJugador(String nombre) {
         try {
@@ -39,21 +53,20 @@ public class PregJugadoresService {
             return new Respuesta(false, "Error obteniendo el jugador.", "getJugador " + ex.getMessage());
         }
     }
-    
-     public Respuesta getJugadores(Long id, String nombre, Long partidasJugadas, Long partidasGanadas, Long cantidadRespuestasGeneral, Long cantidadAcertadasGeneral) {
+
+    public Respuesta getJugadores(Long id, String nombre, Long partidasJugadas, Long partidasGanadas, Long cantidadRespuestasGeneral, Long cantidadAcertadasGeneral) {
         try {
             Query qryJugadores = em.createNamedQuery("PregJugadores.findByParameters", PregJugadores.class);
-            qryJugadores.setParameter("id", "%"+id+"%");
-            qryJugadores.setParameter("nombre", "%"+nombre+"%");
-            qryJugadores.setParameter("partidasJugadas", "%"+partidasJugadas+"%");
-            qryJugadores.setParameter("partidasGanadas", "%"+partidasGanadas+"%");
-            qryJugadores.setParameter("cantidadRespuestasGeneral", "%"+cantidadRespuestasGeneral+"%");
-            qryJugadores.setParameter("cantidadAcertadasGeneral", "%"+cantidadAcertadasGeneral+"%");
-            
+            qryJugadores.setParameter("id", "%" + id + "%");
+            qryJugadores.setParameter("nombre", "%" + nombre + "%");
+            qryJugadores.setParameter("partidasJugadas", "%" + partidasJugadas + "%");
+            qryJugadores.setParameter("partidasGanadas", "%" + partidasGanadas + "%");
+            qryJugadores.setParameter("cantidadRespuestasGeneral", "%" + cantidadRespuestasGeneral + "%");
+            qryJugadores.setParameter("cantidadAcertadasGeneral", "%" + cantidadAcertadasGeneral + "%");
+
             List<PregJugadoresDto> jugadores = new ArrayList<>();
             List<PregJugadores> jugador = qryJugadores.getResultList();
-            for (PregJugadores tempJugadores : jugador) 
-            {
+            for (PregJugadores tempJugadores : jugador) {
                 jugadores.add(new PregJugadoresDto(tempJugadores));
             }
             return new Respuesta(true, "", "", "PregJugadores", jugadores);
@@ -68,95 +81,52 @@ public class PregJugadoresService {
         }
     }
 
-//    public Respuesta getEmpleado(Long id) {
-//        try {
-//            Query qryEmpleado = em.createNamedQuery("Empleado.findByEmpId", Empleado.class);
-//            qryEmpleado.setParameter("id", id);
-//            EmpleadoDto empleadoDto = new EmpleadoDto((Empleado) qryEmpleado.getSingleResult());
-//            return new Respuesta(true, "", "", "Empleado", empleadoDto);
-//        } catch (NoResultException ex) {
-//            return new Respuesta(false, "No existe un empleado el ID ingresadas.", "getEmpleado NoResultException");
-//        } catch (NonUniqueResultException ex) {
-//            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Ocurrio un error al consultar el empleado.", ex);
-//            return new Respuesta(false, "Ocurrio un error al consultar al empleado.", "getEmpleado NonUniqueResultException");
-//        } catch (Exception ex) {
-//            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Error obteniendo al empleado [" + id + "]", ex);
-//            return new Respuesta(false, "Error obteniendo al empleado.", "getEmpleado " + ex.getMessage());
-//        }
-//    }
-//
-//    public Respuesta getEmpleados(String cedula, String nombre, String pApellido, String sApellido) {
-//        try {
-//            Query qryEmpleado = em.createNamedQuery("Empleado.findByParameters", Empleado.class);
-//            qryEmpleado.setParameter("cedula", "%"+cedula+"%");
-//            qryEmpleado.setParameter("nombre", "%"+nombre+"%");
-//            qryEmpleado.setParameter("pApellido", "%"+pApellido+"%");
-//            qryEmpleado.setParameter("sApellido", "%"+sApellido+"%");
-//            
-//            List<EmpleadoDto> empleados = new ArrayList<>();
-//            List<Empleado> empleado = qryEmpleado.getResultList();
-//            for (Empleado tempEmpleado : empleado) 
-//            {
-//                empleados.add(new EmpleadoDto(tempEmpleado));
-//            }
-//            return new Respuesta(true, "", "", "Empleados", empleados);
-//        } catch (NoResultException ex) {
-//            return new Respuesta(false, "No existe ningun usuario con los parametros colocados.", "getEmpleados NoResultException");
-//        } catch (NonUniqueResultException ex) {
-//            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Ocurrio un error al consultar el empleado.", ex);
-//            return new Respuesta(false, "Ocurrio un error al consultar los empleados.", "getEmpleado NonUniqueResultException");
-//        } catch (Exception ex) {
-//            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Error obteniendo los empleados", ex);
-//            return new Respuesta(false, "Error obteniendo los empleados.", "getEmpleados " + ex.getMessage());
-//        }
-//    }
-//
-//    public Respuesta guardarEmpleado(EmpleadoDto empleadoDto) {
-//        try {
-//            et = em.getTransaction();
-//            et.begin();
-//            Empleado empleado;
-//            if (empleadoDto.getId() != null && empleadoDto.getId() > 0) {
-//                empleado = em.find(Empleado.class, empleadoDto.getId());
-//                if (empleado == null) {
-//                    return new Respuesta(false, "No se encontró", "guardarEmpleado NoResultException");
-//                }
-//                empleado.Actualizar(empleadoDto);
-//                empleado = em.merge(empleado);
-//            } else {
-//                empleado = new Empleado(empleadoDto);
-//                em.persist(empleado);
-//            }
-//            et.commit();
-//            return new Respuesta(true, "", "", "Empleado", new EmpleadoDto(empleado));
-//
-//        } catch (Exception ex) {
-//            et.rollback();
-//            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Error guardadndo al empleado", ex);
-//            return new Respuesta(false, "Error guardadndo al empleado.", "guardarEmpleado " + ex.getMessage());
-//        }
-//    }
-//
-//    public Respuesta eliminarEmpleado(Long id) {
-//        try {
-//            et = em.getTransaction();
-//            et.begin();
-//            Empleado empleado;
-//            if (id != null && id > 0) {
-//                empleado = em.find(Empleado.class, id);
-//                if (empleado == null) {
-//                    return new Respuesta(false, "No se encontró", "eliminarEmpleado NoResultException");
-//                }
-//                em.remove(empleado);
-//            } else {
-//                return new Respuesta(false, "No se encontró", "eliminarEmpleado NoResultException");
-//            }
-//            et.commit();
-//            return new Respuesta(true, "", "");
-//        } catch (Exception ex) {
-//            et.rollback();
-//            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Error guardadndo al empleado", ex);
-//            return new Respuesta(false, "Error guardadndo al empleado.", "guardarEmpleado " + ex.getMessage());
-//        }
-//    }
+    public Respuesta guardarJugador(PregJugadoresDto pregJugadoresDto) {
+        try {
+            et = em.getTransaction();
+            et.begin();
+            PregJugadores pregJugadores;
+            if (pregJugadoresDto.getId() != null && pregJugadoresDto.getId() > 0) {
+                pregJugadores = em.find(PregJugadores.class, pregJugadoresDto.getId());
+                if (pregJugadores == null) {
+                    return new Respuesta(false, "No se encontró", "guardarJugador NoResultException");
+                }
+                pregJugadores.Actualizar(pregJugadoresDto);
+                pregJugadores = em.merge(pregJugadores);
+            } else {
+                pregJugadores = new PregJugadores(pregJugadoresDto);
+                em.persist(pregJugadores);
+            }
+            et.commit();
+            return new Respuesta(true, "", "", "PregJugador", new PregJugadoresDto(pregJugadores));
+
+        } catch (Exception ex) {
+            et.rollback();
+            Logger.getLogger(PregJugadoresService.class.getName()).log(Level.SEVERE, "Error guardadndo al jugador", ex);
+            return new Respuesta(false, "Error guardadndo al jugador.", "guardarJugador " + ex.getMessage());
+        }
+    }
+
+    public Respuesta eliminarJugador(String nombre) {
+        try {
+            et = em.getTransaction();
+            et.begin();
+            PregJugadores pregJugadores;
+            if (nombre != null && nombre.isEmpty() && nombre.isBlank()) {
+                pregJugadores = em.find(PregJugadores.class, nombre);
+                if (pregJugadores == null) {
+                    return new Respuesta(false, "No se encontró", "eliminarJugador NoResultException");
+                }
+                em.remove(pregJugadores);
+            } else {
+                return new Respuesta(false, "No se encontró", "eliminarJugador NoResultException");
+            }
+            et.commit();
+            return new Respuesta(true, "", "");
+        } catch (Exception ex) {
+            et.rollback();
+            Logger.getLogger(PregJugadoresService.class.getName()).log(Level.SEVERE, "Error eliminando al jugador", ex);
+            return new Respuesta(false, "Error eliminando al jugador.", "eliminarJugador " + ex.getMessage());
+        }
+    }
 }
