addc9f01bba4d292b8cf1a0bb8ed6b920f0ab985
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-06-01 10:29:37 -0600

    Add more things in service
    Co-Authored-By: Matteo Vargas <142123012+matteoquesada@users.noreply.github.com>

--- preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/service/PregRespuestasService.java
+++ preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/service/PregRespuestasService.java
@@ -29,17 +29,17 @@ public class PregRespuestasService {
         } catch (NoResultException ex) {
             return new Respuesta(false, "No existe la respuesta consultada.", "getRespuesta NoResultException");
         } catch (NonUniqueResultException ex) {
-            Logger.getLogger(PregCategoriasService.class.getName()).log(Level.SEVERE, "Ocurrio un error al consultar la respuesta.", ex);
+            Logger.getLogger(PregRespuestasService.class.getName()).log(Level.SEVERE, "Ocurrio un error al consultar la respuesta.", ex);
             return new Respuesta(false, "Ocurrio un error al consultar la respuesta.", "getJugador NonUniqueResultException");
         } catch (Exception ex) {
-            Logger.getLogger(PregCategoriasService.class.getName()).log(Level.SEVERE, "Error obteniendo la respuesta de id:  [" + id + "]", ex);
+            Logger.getLogger(PregRespuestasService.class.getName()).log(Level.SEVERE, "Error obteniendo la respuesta de id:  [" + id + "]", ex);
             return new Respuesta(false, "Error obteniendo la repuesta.", "getRespuesta " + ex.getMessage());
         }
     }
 
     public Respuesta getRespuestas(PregPreguntas respIdpre) {
         try {
-           Query qryRespuesta = em.createNamedQuery("PregRespuestas.findByRespIdpre", PregRespuestas.class);
+            Query qryRespuesta = em.createNamedQuery("PregRespuestas.findByRespIdpre", PregRespuestas.class);
             qryRespuesta.setParameter("respIdpre", respIdpre);
 
             List<PregRespuestasDto> respuestas = new ArrayList<>();
@@ -51,39 +51,67 @@ public class PregRespuestasService {
         } catch (NoResultException ex) {
             return new Respuesta(false, "No existe ninguna respuesta con los parametros colocados.", "getRespuestas NoResultException");
         } catch (NonUniqueResultException ex) {
-            Logger.getLogger(PregJugadoresService.class.getName()).log(Level.SEVERE, "Ocurrio un error al consultar la respuesta.", ex);
+            Logger.getLogger(PregRespuestasService.class.getName()).log(Level.SEVERE, "Ocurrio un error al consultar la respuesta.", ex);
             return new Respuesta(false, "Ocurrio un error al consultar las respuestas.", "getRespuestas NonUniqueResultException");
         } catch (Exception ex) {
-            Logger.getLogger(PregJugadoresService.class.getName()).log(Level.SEVERE, "Error obteniendo las respuestas", ex);
+            Logger.getLogger(PregRespuestasService.class.getName()).log(Level.SEVERE, "Error obteniendo las respuestas", ex);
             return new Respuesta(false, "Error obteniendo las respuestas.", "getRespuestas " + ex.getMessage());
         }
     }
 
-//    public Respuesta guardarRespuesta(PregRespuestasDto pregRespuestasDto) {
-//        try {
-//            et = em.getTransaction();
-//            et.begin();
-//            PregJugadores pregJugadores;
-//            if (pregJugadoresDto.getId() != null && pregJugadoresDto.getId() > 0) {
-//                pregJugadores = em.find(PregJugadores.class, pregJugadoresDto.getId());
-//                if (pregJugadores == null) {
-//                    return new Respuesta(false, "No se encontró", "guardarJugador NoResultException");
-//                }
-//                pregJugadores.Actualizar(pregJugadoresDto);
-//                pregJugadores = em.merge(pregJugadores);
-//            } else {
-//                pregJugadores = new PregJugadores(pregJugadoresDto);
-//                em.persist(pregJugadores);
-//            }
-//            et.commit();
-//            return new Respuesta(true, "", "", "PregJugador", new PregJugadoresDto(pregJugadores));
-//
-//        } catch (Exception ex) {
-//            et.rollback();
-//            Logger.getLogger(PregJugadoresService.class.getName()).log(Level.SEVERE, "Error guardadndo al jugador", ex);
-//            return new Respuesta(false, "Error guardadndo al jugador.", "guardarJugador " + ex.getMessage());
-//        }
-//    }
+    public Respuesta guardarRespuesta(PregRespuestasDto pregRespuestasDto) {
+        try {
+            et = em.getTransaction();
+            et.begin();
+            PregRespuestas pregRespuestas;
+            if (pregRespuestasDto.getId() != null && pregRespuestasDto.getId() > 0) {
+                pregRespuestas = em.find(PregRespuestas.class, pregRespuestasDto.getId());
+                if (pregRespuestas == null) {
+                    return new Respuesta(false, "No se encontró", "guardarRespuesta NoResultException");
+                }
+                pregRespuestas.Actualizar(pregRespuestasDto);
+                pregRespuestas = em.merge(pregRespuestas);
+            } else {
+                pregRespuestas = new PregRespuestas(pregRespuestasDto);
+                em.persist(pregRespuestas);
+            }
+            et.commit();
+            return new Respuesta(true, "", "", "PregRespuesta", new PregRespuestasDto(pregRespuestas));
+
+        } catch (Exception ex) {
+            et.rollback();
+            Logger.getLogger(PregRespuestasService.class.getName()).log(Level.SEVERE, "Error guardando la respuesta", ex);
+            return new Respuesta(false, "Error guardando la respuesta.", "guardarRespuesta " + ex.getMessage());
+        }
+    }
+
+    public Respuesta guardarRespuestas(List<PregRespuestasDto> respuestas) {
+        try {
+            for (PregRespuestasDto tempRespuestas : respuestas) {
+                et = em.getTransaction();
+                et.begin();
+                PregRespuestas pregRespuestas;
+                if (tempRespuestas.getId() != null && tempRespuestas.getId() > 0) {
+                    pregRespuestas = em.find(PregRespuestas.class, tempRespuestas.getId());
+                    if (pregRespuestas == null) {
+                        return new Respuesta(false, "No se encontró", "guardarRespuesta NoResultException");
+                    }
+                    pregRespuestas.Actualizar(tempRespuestas);
+                    pregRespuestas = em.merge(pregRespuestas);
+                } else {
+                    pregRespuestas = new PregRespuestas(tempRespuestas);
+                    em.persist(pregRespuestas);
+                }
+                et.commit();
+                return new Respuesta(true, "", "", "PregRespuesta", new PregRespuestasDto(pregRespuestas));
+            }
+            return new Respuesta(true, "", "", "PregRespuestas", respuestas);
+        } catch (Exception ex) {
+            et.rollback();
+            Logger.getLogger(PregRespuestasService.class.getName()).log(Level.SEVERE, "Error guardando la respuesta", ex);
+            return new Respuesta(false, "Error guardando la respuesta.", "guardarRespuesta " + ex.getMessage());
+        }
+    }
 
 //    public Respuesta eliminarJugador(String nombre) {
 //        try {
