7b9c1ee488cdf95cc50202799b53150e22b6f0dc
EstebanJavierGranadosSibaja <esteban.granados.sibaja@est.una.ac.cr>
2024-06-02 21:08:15 -0600

    Add small things
    Co-Authored-By: Matteo Vargas <142123012+matteoquesada@users.noreply.github.com>

--- preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/controller/ListaPreguntasController.java
+++ preguntadosPackage/src/main/java/cr/ac/una/preguntadospackage/controller/ListaPreguntasController.java
@@ -4,11 +4,19 @@
  */
 package cr.ac.una.preguntadospackage.controller;
 
+import cr.ac.una.preguntadospackage.model.PregCategoriasDto;
+import cr.ac.una.preguntadospackage.model.PregJugadoresDto;
+import cr.ac.una.preguntadospackage.model.PregPreguntasDto;
+import cr.ac.una.preguntadospackage.service.PregPreguntasService;
 import cr.ac.una.preguntadospackage.util.FlowController;
+import cr.ac.una.preguntadospackage.util.Respuesta;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -34,31 +42,44 @@ public class ListaPreguntasController extends Controller implements Initializabl
     @FXML
     private MFXTextField txtCantidadAciertos;
     @FXML
+    private MFXTextField txtEstado;
+    @FXML
     private MFXButton btnFiltrar;
     @FXML
-    private TableView<?> tbvPreguntas;
+    private TableView<PregPreguntasDto> tbvPreguntas;
     @FXML
-    private TableColumn<?, ?> tbcId;
+    private TableColumn<PregPreguntasDto, String> tbcId;
     @FXML
-    private TableColumn<?, ?> tbcCategoria;
+    private TableColumn<PregCategoriasDto, String> tbcCategoria;
     @FXML
-    private TableColumn<?, ?> tbcEnunciado;
+    private TableColumn<PregPreguntasDto, String> tbcEnunciado;
     @FXML
-    private TableColumn<?, ?> tbcCantidadRespuestas;
+    private TableColumn<PregPreguntasDto, String> tbcCantidadRespuestas;
     @FXML
-    private TableColumn<?, ?> tbcCantidadAcertadas;
+    private TableColumn<PregPreguntasDto, String> tbcCantidadAcertadas;
+    @FXML
+    private TableColumn<PregPreguntasDto, Boolean> tbcEstado;
     @FXML
     private MFXButton btnVolver;
     @FXML
     private MFXButton btnAceptar11;
+
+    private ObservableList<PregPreguntasDto> preguntasList;
+
+    private PregPreguntasDto resultado;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        tbcId.setCellValueFactory(cd -> cd.getValue().id);
+        tbcCategoria.setCellValueFactory(cd -> cd.getValue().categoria);
+        tbcEnunciado.setCellValueFactory(cd -> cd.getValue().textoPregunta);
+        tbcCantidadRespuestas.setCellValueFactory(cd -> cd.getValue().cantidadRespuestas);
+        tbcCantidadAcertadas.setCellValueFactory(cd -> cd.getValue().cantidadAciertos);
+        tbcEstado.setCellValueFactory(cd -> cd.getValue().estado);
+    }
 
     @Override
     public void initialize() {
@@ -67,19 +88,32 @@ public class ListaPreguntasController extends Controller implements Initializabl
 
     @FXML
     private void onActionFiltrar(ActionEvent event) {
+        PregPreguntasService empService = new PregPreguntasService();
+        Respuesta respuesta = empService.getPreguntas(txtId.getText().toUpperCase(), tbcEnunciado.getText().toUpperCase(), tbcCantidadRespuestas.getText().toUpperCase(), tbcCantidadAcertadas.getText().toUpperCase(), tbcEstado.getText().toUpperCase());
+        if (respuesta.getEstado()) {
+            preguntasList = FXCollections.observableArrayList((List<PregPreguntasDto>) respuesta.getResultado("PregPreguntas"));
+            tbvPreguntas.setItems(preguntasList);
+            tbvPreguntas.refresh();
+
+        }
     }
 
     @FXML
     private void onMousePressedTbvPreguntas(MouseEvent event) {
+        if (event.isPrimaryButtonDown() && event.getClickCount() == 2) {
+            onAcionAceptar(null);
+        }
     }
 
     @FXML
     private void onAcionVolver(ActionEvent event) {
-         FlowController.getInstance().goView("MenuView");
+        FlowController.getInstance().goView("MenuView");
     }
 
     @FXML
     private void onAcionAceptar(ActionEvent event) {
+        resultado = (PregPreguntasDto) tbvPreguntas.getSelectionModel().getSelectedItem();
+        //getStage().close();
     }
-    
+
 }
